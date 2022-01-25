a3d86984f342c90d5e09a531644fe98bc8c96bcd
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-01-25 11:08:10 -0300

    Create flutter plugin template.

---
+++ ios/Classes/MyFlutterPlugin.m
@@ -0,0 +1,15 @@
+#import "MyFlutterPlugin.h"
+#if __has_include(<my_flutter_plugin/my_flutter_plugin-Swift.h>)
+#import <my_flutter_plugin/my_flutter_plugin-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "my_flutter_plugin-Swift.h"
+#endif
+
+@implementation MyFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftMyFlutterPlugin registerWithRegistrar:registrar];
+}
+@end
