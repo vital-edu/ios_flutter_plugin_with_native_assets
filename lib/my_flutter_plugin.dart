391eedb3a54cf9bf2825385c0c8c8f2c8b949aa0
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-01-25 11:42:18 -0300

    Add dart code in the plugin to invoke the method in the native platform.

--- lib/my_flutter_plugin.dart
+++ lib/my_flutter_plugin.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -9,5 +8,9 @@ class MyFlutterPlugin {
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static void playSound() {
+    _channel.invokeMethod('playSound');
   }
 }
